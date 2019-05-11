9e673ef20238b62c628459f9b3232cfd3628fe0c
Vccentric <35283237+Vccentric@users.noreply.github.com>
2019-05-10 21:42:00 -0700

    Updates to the layout and CSS of the 'ConsultationForm' component.

--- src/design-1/components/ConsultationForm.js
+++ src/design-1/components/ConsultationForm.js
@@ -2,23 +2,22 @@ import React from "react";
 
 const ConsultationForm = () => {
     return (
-        <div id="consultation">
-            <h3 className="title">Get a free consultation.</h3>
-            <p className="description">We'd love to learn more about your business needs and discuss how we can help you deliever the results you want.</p>
-            <input type="text" id="fName" name="fName" placeholder="First Name" />
-            <br />
-            <input type="text" id="lName" name="lName" placeholder="Last Name" />
-            <br />
-            <input type="text" id="wEmail" name="wEmail" placeholder="Work Email" />
-            <br />
-            <input type="text" id="cName" name="cName" placeholder="Company Name" />
-            <br />
-            <select name="topic">
-                <option value="">What are you interested in?</option>
-                <option value="data">Data</option>
-                <option value="price">Pricing</option>
-                <option value="promo">Promotions</option>
-            </select>
+        <div id="consultation-form">
+            <div className="container">
+                <h2 className="title">GET A FREE CONSULTATION</h2>
+                <p className="description">We'd love to learn more about your business needs and discuss how we can help you deliever the results you want.</p>
+                <input type="text" id="fName" name="fName" placeholder="First Name" />
+                <input type="text" id="lName" name="lName" placeholder="Last Name" />
+                <input type="text" id="wEmail" name="wEmail" placeholder="Work Email" />
+                <input type="text" id="cName" name="cName" placeholder="Company Name" />
+                <select name="topic">
+                    <option value="">What are you interested in?</option>
+                    <option value="data">Data</option>
+                    <option value="price">Pricing</option>
+                    <option value="promo">Promotions</option>
+                </select>
+                <button type="button" className="button submit">SUBMIT</button>
+            </div>
         </div>
     );
 }
