04c47254ccb2d964ca9cf6fa5cc1ac08fbc287a1
Vccentric <35283237+Vccentric@users.noreply.github.com>
2019-05-10 21:27:21 -0700

    1. Layout updates to 'Footer' and 'NewsletterForm' components. 2. CSS updated to 'Footer' and 'NewsletterForm' components.

--- src/design-1/components/Footer.js
+++ src/design-1/components/Footer.js
@@ -5,25 +5,27 @@ import NewsletterForm from "./NewsletterForm";
 const Footer = () => {
     return (
         <footer id="footer">
-            <ul className="container">
-                <li className="category">SERVICES</li>
-                <li>Compliance</li>
-                <li>Market Intelligence</li>
-                <li>Optimization</li>
-            </ul>
-            <ul className="container">
-                <li className="category">COMPANY</li>
-                <li>About Us</li>
-                <li>Careers</li>
-                <li>Resources</li>
-            </ul>
-            <ul className="container">
-                <li className="category">SUPPORT</li>
-                <li>Contact Us</li>
-                <li>Support</li>
-                <li>Resources</li>
-            </ul>
-            <NewsletterForm />
+            <div className="container max">
+                <ul className="container">
+                    <li className="category">SERVICES</li>
+                    <li>Compliance</li>
+                    <li>Market Intelligence</li>
+                    <li>Optimization</li>
+                </ul>
+                <ul className="container">
+                    <li className="category">COMPANY</li>
+                    <li>About Us</li>
+                    <li>Careers</li>
+                    <li>Resources</li>
+                </ul>
+                <ul className="container">
+                    <li className="category">SUPPORT</li>
+                    <li>Contact Us</li>
+                    <li>Support</li>
+                    <li>Resources</li>
+                </ul>
+                <NewsletterForm />
+            </div>
         </footer>
     );
 }
--- src/design-1/components/NewsletterForm.js
+++ src/design-1/components/NewsletterForm.js
@@ -5,10 +5,8 @@ const NewsletterForm = () => {
         <div id="newsletter-form">
             <h3 className="title">SUBSCRIBE TO OUR NEWSLETTER</h3>
             <p className="description">Don't miss out on our latest news, posts, and blogs.</p>
-            <div className="container row">
-                <input type="text" id="emailForNewsletter" name="emailForNewsletter" placeholder="Email Address" />
-                <button type="button" className="button submit">SUBSCRIBE</button>
-            </div>
+            <input type="text" id="emailForNewsletter" name="emailForNewsletter" placeholder="Email Address" />
+            <button type="button" className="button submit">SUBSCRIBE</button>
         </div>
     );
 }
