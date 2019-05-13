70b80fd94fe1fdca63c982718a6ff6c0a2711625
Vccentric <35283237+Vccentric@users.noreply.github.com>
2019-05-12 21:00:45 -0700

    Updated to the layout and CSS of the 'HowItWorksSection' component.

--- src/design-1/components/HowItWorksSection.js
+++ src/design-1/components/HowItWorksSection.js
@@ -3,15 +3,29 @@ import React from "react";
 const HowItWorksSection = () => {
     return (
         <div id="how-it-works-section">
-            <ul className="container how-it-works-points">
-                <li className="point">30+ BRANDS TRACKED</li>
-                <li className="point">100M+ SKUS MONITORED</li>
-                <li className="point">700K CROWDSOURCED SHOPPERS</li>
-                <li className="point">5+ YEARS OF PRODUCT HISTORY</li>
-            </ul>
-            <div className="">
-                <h3 className="title">How It Works</h3>
-                <p className="description">Demo gathers and analyzes billions of data points across webstores, marketplaces, brick and mortar stores, emails, and social media. Using a unique combination of data science and human validation, Wiser generates powerful insights and blends them with workflow software to drive measurable cost and time savings, profitable growth, and team empowerment.</p>
+            <div className="container max row">
+                <ul className="container row how-it-works-points">
+                    <li className="point">
+                        <span className="num">30+</span>
+                        <span>BRANDS TRACKED</span>
+                    </li>
+                    <li className="point">
+                        <span className="num">100+</span>
+                        <span>SKUS MONITORED</span>
+                    </li>
+                    <li className="point">
+                        <span className="num">700+</span>
+                        <span>CROWDSOURCED SHOPPERS</span>
+                    </li>
+                    <li className="point">
+                        <span className="num">5+</span>
+                        <span>YEARS OF PRODUCT HISTORY</span>
+                    </li>
+                </ul>
+                <div className="container column how-it-works-info">
+                    <h2 className="title">How It Works</h2>
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in libero justo. Nullam ut dictum ante, eget semper massa. Curabitur ante mi, eleifend nec mi et, molestie dapibus massa. Maecenas molestie felis bibendum efficitur placerat. Phasellus ultrices efficitur leo, vel sagittis dui iaculis at. Vestibulum accumsan tortor eget tortor sollicitudin tincidunt. Pellentesque enim tellus, tempor id tincidunt a, feugiat et lectus. Donec bibendum tincidunt dui quis efficitur. Sed dapibus mauris eu quam blandit, in vulputate ipsum vestibulum. Praesent fermentum pulvinar placerat.</p>
+                </div>
             </div>
         </div>
     );
