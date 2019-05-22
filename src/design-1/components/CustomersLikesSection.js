a4f375bb69cc1a52e541b779b05b6acd20ab9f52
Vccentric <35283237+Vccentric@users.noreply.github.com>
2019-05-22 14:21:08 -0700

    1. Update the text content in the components: 'CustomersLikesSection' and 'ServicesSection'. 2. Small CSS updates.

--- src/design-1/components/CustomersLikesSection.js
+++ src/design-1/components/CustomersLikesSection.js
@@ -8,27 +8,27 @@ const CustomersLikesSection = () => {
                 <ul className="container customers-likes-list">
                     <div className="customers-like">
                         <h3>Billions of Data Points</h3>
-                        <p>The most comprehensive coverage both online and in-store with more data points added every day.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at ex nec enim tempus auctor. Phasellus a quam quis ligula rutrum tristique. </p>
                     </div>
                     <div className="customers-like">
                         <h3>World Class Support</h3>
-                        <p>Every customer is assigned a dedicated account manager to ensure success.</p>
+                        <p>Quisque in imperdiet nisl, non scelerisque nunc. Sed laoreet felis eu orci gravida, ac sagittis erat volutpat. Aenean posuere, ante auctor aliquet vestibulum, purus purus molestie leo, consectetur tempor mauris eros nec tellus.</p>
                     </div>
                     <div className="customers-like">
                         <h3>Actionable Insights</h3>
-                        <p>Easily analyze big data to make confident decisions that drive impactful results.</p>
+                        <p>Nullam fringilla semper nunc ut vulputate. Etiam id quam egestas, posuere ipsum et, lacinia erat. Mauris sodales gravida sem, eu tempus augue egestas in. Sed non elit eget ante viverra accumsan at eget urna.</p>
                     </div>
                     <div className="customers-like">
                         <h3>Omnichannel Coverage</h3>
-                        <p>A complete view of the retail landscape on and offline to optimize your entire business.</p>
+                        <p>Integer rutrum fermentum ante viverra dapibus. Proin pretium, nisl eu vulputate suscipit, ex justo vestibulum est, et lacinia massa diam sit amet nisi. Sed sed lectus erat. Cras eu convallis massa.</p>
                     </div>
                     <div className="customers-like">
                         <h3>High Quality Data</h3>
-                        <p>We combine the power of data science with human validation to ensure the highest quality and accuracy.</p>
+                        <p>Donec placerat convallis sodales. In posuere mattis erat nec auctor. Etiam finibus sit amet mi sit amet elementum. In sit amet tellus interdum, vehicula erat eu, finibus enim.</p>
                     </div>
                     <div className="customers-like">
                         <h3>Increased Efficiency</h3>
-                        <p>Automated data collection and workflow processes for measurable cost and time savings.</p>
+                        <p>Nam tempus in lorem aliquam pellentesque. Nullam non libero sit amet libero faucibus vestibulum. Integer vel consectetur nisl, ac aliquet velit. Pellentesque rutrum in tortor sed tincidunt.</p>
                     </div>
                 </ul>
             </div>
--- src/design-1/components/ServicesSection.js
+++ src/design-1/components/ServicesSection.js
@@ -8,17 +8,17 @@ const ServicesSection = () => {
                 <div className="container row services">
                     <div className="container column service">
                         <h3 className="title">Market Intelligence</h3>
-                        <p className="description">Comprehensive market intelligence for ultimate visibility into your competitive landscape across the spectrum of pricing, assortment, and promotions.</p>
+                        <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet est nec est imperdiet auctor. Nunc cursus nibh sapien, ut tincidunt enim ullamcorper et. Proin vel sapien efficitur, rutrum leo vel, luctus ipsum. Integer placerat porta felis, non dignissim diam dictum ut. Duis ac neque venenatis, luctus orci quis, luctus velit. Sed suscipit enim et libero sollicitudin egestas. Vestibulum porttitor iaculis euismod. Pellentesque et tortor elit.</p>
                         <span>LEARN MORE</span>
                     </div>
                     <div className="container column service">
                         <h3 className="title">Optimization</h3>
-                        <p className="description">Automate online and in-store data collection, analysis, and workflows to capitalize on opportunities to optimize your business.</p>
+                        <p className="description">Nam ac purus varius, maximus ex quis, consectetur magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras feugiat aliquam dictum. Suspendisse sagittis pulvinar velit ac faucibus. In in dignissim orci, at blandit enim. Nulla enim ante, malesuada quis est vitae, mattis tincidunt mauris. Duis id augue ut turpis eleifend sagittis vitae ut dui. Aenean et diam ac magna pharetra porttitor.</p>
                         <span>LEARN MORE</span>
                     </div>
                     <div className="container column service">
                         <h3 className="title">Compliance</h3>
-                        <p className="description">Protect your brand by ensuring pricing and merchandising compliance across your entire reseller network, both online and in-store.</p>
+                        <p className="description">Cras eu turpis et dolor volutpat venenatis id ac mauris. In et nisl et ligula consequat bibendum. Nunc vestibulum auctor turpis, in hendrerit lacus sagittis in. Vestibulum eget quam nec elit blandit semper. Cras vitae nisl vel ligula consequat viverra. Nulla at mi pharetra, scelerisque mauris non, auctor nunc. Cras consequat pretium tortor eu vestibulum. Aliquam convallis tellus nec quam varius, cursus efficitur nunc semper.</p>
                         <span>LEARN MORE</span>
                     </div>
                 </div>
